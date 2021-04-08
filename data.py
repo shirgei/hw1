2d131b35342363d1a3ac6c3a824fb384064bb7a4
Shir <shir.g@campus.technion.ac.il>
2021-04-08 20:15:00 +0300

    new

--- data.py
+++ data.py
@@ -4,19 +4,19 @@ import pandas
 def load_data(path, features):
     df = pandas.read_csv(path)
     data = df.to_dict(orient="list")
+    for key in list(data.keys()):
+        if key not in features:
+            data.pop(key)
     return data
 
 
 def filter_by_feature(data, feature, values):
-   #data1 = {}
-   #data2 = {}
-   all_features = list(data.keys())
-   print(all_features)
-   """for all_features in data:
-        data1.setdefault(all_features, [])
-        data2.setdefault(all_features, [])
-    data1 = {"cnt":[], "season":[], "is_holiday":[],"hum":[],"t1":[]}
-    data2 = {"cnt":[], "season":[], "is_holiday":[],"hum":[],"t1":[]}
+    data1 = {}
+    data2 = {}
+    all_features = list(data.keys())
+    for key in all_features:
+        data1.setdefault(key, [])
+        data2.setdefault(key, [])
     lst = data[feature]
     for index, value in enumerate(lst):
         if value in values:
@@ -25,13 +25,13 @@ def filter_by_feature(data, feature, values):
         else:
             for category in all_features:
                 data2[category].append(data[category][index])
-    return data1, data2"""
+    return data1, data2
 
 
 def print_details(data, features, statistic_functions):
     for category in features:
         lst = data[category]
-        results=[]
+        results = []
         print(category, end=": ")
         for method in statistic_functions:
             results.append(str(method(lst)))
